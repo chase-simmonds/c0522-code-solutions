43ed4db1c72497412c34a579afec1ee0aea48a4e
Chase Simmonds <chasexsimmonds@gmail.com>
2022-05-16 17:36:22 +0000

    Changed typeofvalues on the console log

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -6,9 +6,9 @@ console.log('value of Full Name: ', fullName);
 console.log('value of Am I Cool: ', isCool);
 console.log('value of Total Pets: ', totalPets);
 
-console.log('typeof fullName:', typeof 'Chase Simmonds');
-console.log('type of isCool:', typeof true);
-console.log('type of totalPets:', typeof 2);
+console.log('typeof fullName:', typeof fullName);
+console.log('type of isCool:', typeof isCool);
+console.log('type of totalPets:', typeof totalPets);
 
 var unknown = null;
 console.log('value of unknown:', unknown);
