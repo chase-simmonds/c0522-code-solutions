c76a032fade50913b4eaccb1a6d5c80911b63af1
Chase Simmonds <chasexsimmonds@gmail.com>
2022-05-16 17:26:52 +0000

    javascript-primitives-and-variables - Chase Simmonds

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Chase Simmonds';
+var isCool = true;
+var totalPets = 2;
+
+console.log('value of Full Name: ', fullName);
+console.log('value of Am I Cool: ', isCool);
+console.log('value of Total Pets: ', totalPets);
+
+console.log('typeof fullName:', typeof 'Chase Simmonds');
+console.log('type of isCool:', typeof true);
+console.log('type of totalPets:', typeof 2);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
