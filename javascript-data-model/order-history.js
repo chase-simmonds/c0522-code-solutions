932f22bc1ed861d880da5ade5e4e1e1f7d85733a
Chase Simmonds <chasexsimmonds@gmail.com>
2022-05-17 20:13:19 +0000

    javascript-data-model - Chase Simmonds

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,58 @@
+var orderHistory = [
+  {
+    orderNumber: '113-2883177-2648248',
+    shipTo: 'JS Masher',
+    total: 138.93,
+    orderPlaced: 'July 3, 2020',
+    orderDelivered: 'July 5, 2020',
+    items: [
+      {
+        itemName: 'The Art of SQL',
+        price: 33.99
+      },
+      {
+        itemName: 'GameCube Controller - Super Smash Bros Edition',
+        price: 94.95
+      }
+    ]
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    shipTo: 'JS Masher',
+    total: 17.22,
+    orderPlaced: 'July 4, 2020',
+    orderDelivered: 'July 7, 2020',
+    items: [
+      {
+        itemName: 'GameCube Controller Adapter',
+        price: 15.98
+      }
+    ]
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    shipTo: 'JS Masher',
+    total: 44.53,
+    orderPlaced: 'July 19, 2020',
+    orderDelivered: 'July 20, 2020',
+    items: [
+      {
+        itemName: 'The Timeless Way of Building',
+        price: 41.33
+      }
+    ]
+  },
+  {
+    orderNumber: '114-3941689-8772232',
+    shipTo: 'JS Masher',
+    total: 34.00,
+    orderPlaced: 'August 4, 2020',
+    orderDelivered: 'August 8, 2020',
+    items: [
+      {
+        itemName: 'Javascript for Impatient Programmers',
+        price: 31.55
+      }
+    ]
+  }
+];
