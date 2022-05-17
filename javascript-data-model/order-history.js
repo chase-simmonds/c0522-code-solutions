b6487a911e94d6390344a3bdb0d46ae520f0d128
Chase Simmonds <chasexsimmonds@gmail.com>
2022-05-17 23:37:09 +0000

    added missing order information

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,31 +1,18 @@
 var orderHistory = [
   {
-    orderNumber: '113-2883177-2648248',
+    orderNumber: '114-3941689-8772232',
     shipTo: 'JS Masher',
-    total: 138.93,
-    orderPlaced: 'July 3, 2020',
-    orderDelivered: 'July 5, 2020',
+    total: 34,
+    orderPlaced: '08/04/2020',
+    orderDelivered: '08/08/2020',
     items: [
       {
-        itemName: 'The Art of SQL',
-        price: 33.99
-      },
-      {
-        itemName: 'GameCube Controller - Super Smash Bros Edition',
-        price: 94.95
-      }
-    ]
-  },
-  {
-    orderNumber: '114-2875557-9059409',
-    shipTo: 'JS Masher',
-    total: 17.22,
-    orderPlaced: 'July 4, 2020',
-    orderDelivered: 'July 7, 2020',
-    items: [
-      {
-        itemName: 'GameCube Controller Adapter',
-        price: 15.98
+        type: 'book',
+        title: 'JavaScript for Impatient Programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnUntil: '09/07/2020',
+        price: 31.55,
+        imageUrl: ''
       }
     ]
   },
@@ -33,25 +20,56 @@ var orderHistory = [
     orderNumber: '113-9984268-1280257',
     shipTo: 'JS Masher',
     total: 44.53,
-    orderPlaced: 'July 19, 2020',
-    orderDelivered: 'July 20, 2020',
+    orderPlaced: '07/19/2020',
+    orderDelivered: '07/20/2020',
     items: [
       {
-        itemName: 'The Timeless Way of Building',
-        price: 41.33
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnUntil: '08/19/2020',
+        price: 41.33,
+        imageUrl: ''
       }
     ]
   },
   {
-    orderNumber: '114-3941689-8772232',
+    orderNumber: '114-2875557-9059409',
     shipTo: 'JS Masher',
-    total: 34.00,
-    orderPlaced: 'August 4, 2020',
-    orderDelivered: 'August 8, 2020',
+    total: 17.22,
+    orderPlaced: '07/04/2020',
+    orderDelivered: '07/07/2020',
     items: [
       {
-        itemName: 'Javascript for Impatient Programmers',
-        price: 31.55
+        type: 'hardware',
+        title: 'GameCube Controller Adapter',
+        returnUntil: '08/05/2020',
+        price: 15.98,
+        imageUrl: ''
+      }
+    ]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    shipTo: 'JS Masher',
+    total: 34.00,
+    orderPlaced: '07/03/2020',
+    orderDelivered: '07/05/2020',
+    items: [
+      {
+        type: 'hardware',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnUntil: '08/04/2020',
+        price: 94.95,
+        imageUrl: ''
+      },
+      {
+        type: 'book',
+        title: 'The Art of SQL',
+        author: 'Faroult, Stephane',
+        returnUntil: '08/04/2020',
+        price: 33.99,
+        imageUrl: ''
       }
     ]
   }
