ba6d0ba1f8e20d6e5d3a7741d2feb0c5881348ad
Chase Simmonds <chasexsimmonds@gmail.com>
2022-05-23 18:14:47 +0000

    dom-querying - Chase Simmonds

---
+++ dom-querying/main.js
@@ -0,0 +1,21 @@
+console.log('hello, world');
+
+var $heading = document.querySelector('h1');
+console.log($heading);
+
+console.dir($heading);
+
+var $explanation = document.getElementById('explanation');
+console.log($explanation);
+
+console.dir($explanation);
+
+var $hint = document.getElementsByClassName('hint');
+console.log($hint);
+
+console.dir($hint);
+
+document.querySelectorAll('p');
+
+var $links = document.getElementsByClassName('example-link');
+console.log($links);
