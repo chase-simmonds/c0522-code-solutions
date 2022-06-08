012243e9ba3ee3122bd35479fbf1f542cbcf44b4
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-08 19:34:43 +0000

    Updated to include count variable

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,10 +1,14 @@
 var $countdownDisplay = document.querySelector('.countdown-display');
+var count = 4;
+var countdownId = null;
 
 function countdown() {
-  if ($countdownDisplay.textContent > 1) {
-    $countdownDisplay.textContent = $countdownDisplay.textContent - 1;
+  count--;
+  if (count > 0) {
+    $countdownDisplay.textContent = count;
   } else {
     $countdownDisplay.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(countdownId);
   }
 }
 
