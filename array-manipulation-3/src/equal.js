382b4b1bc5cb04a4d29b69fcd5e4d065fc8617dc
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-27 05:25:06 +0000

    not complete

---
+++ array-manipulation-3/src/equal.js
@@ -0,0 +1,15 @@
+/* exported equal */
+function equal(first, second) {
+  if (first === second) {
+    return true;
+  }
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (var i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
