dfcd9cbeb2876fc8b1c46b3106644e7a185dcd7a
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-28 04:59:14 +0000

    completed flatten

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,16 @@
 /* exported flatten */
+
+function flatten(array) {
+  var flatArray = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var k = 0; k < array[i].length; k++) {
+      if (Array.isArray(array[i])) {
+        flatArray.push(array[i][k]);
+      }
+    }
+    if (!Array.isArray(array[i])) {
+      flatArray.push(array[i]);
+    }
+  }
+  return flatArray;
+}
