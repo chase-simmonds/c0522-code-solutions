0b164f37e1c553513fc50cb990dbef67ad933b3a
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-30 21:49:11 +0000

    added \n to content variables

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const content = process.argv[2];
 
-fs.writeFile('note.txt', content, err => {
+fs.writeFile('note.txt', content + '\n', err => {
   if (err) {
     console.error(err);
   }
--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const randomNum = Math.random().toString();
 
-fs.writeFile('random.txt', randomNum, err => {
+fs.writeFile('random.txt', randomNum + '\n', err => {
   if (err) {
     console.error(err);
   }
