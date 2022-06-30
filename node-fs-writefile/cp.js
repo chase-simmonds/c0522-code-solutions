26971a75b81b8db1463f3986589d6c7fd5bc744c
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-30 20:27:05 +0000

    changed cp.js to arrow functions

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const copiedFile = process.argv[2];
 const pastedFile = process.argv[3];
 
-fs.readFile(copiedFile, 'utf8', readingFile);
-
-function readingFile(error, data) {
+const readingFile = (error, data) => {
   if (error) {
     console.error(error);
   } else {
     fs.writeFile(pastedFile, data, 'utf8', writeFile);
   }
-}
+};
 
-function writeFile(error) {
+fs.readFile(copiedFile, 'utf8', readingFile);
+
+const writeFile = error => {
   if (error) {
     console.error(error);
   }
-}
+};
