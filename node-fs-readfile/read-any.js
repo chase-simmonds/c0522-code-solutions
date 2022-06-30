4f505b96d77f812cf663cb7ef972c55ac205be4e
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-30 19:01:15 +0000

    changed read-any to arrow function

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const filename = process.argv[2];
 
-fs.readFile(filename, 'utf8', function (err, data) {
+fs.readFile(filename, 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
