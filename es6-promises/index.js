d7a710b818088d04dd86c61f4ca5e00dd34ce8c8
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-08 18:34:50 +0000

    changed value to message in then argument

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,8 +2,8 @@ const takeAChance = require('./take-a-chance');
 
 const promise1 = takeAChance('Chase');
 
-promise1.then(value => {
-  console.log(value);
+promise1.then(message => {
+  console.log(message);
 });
 
 promise1.catch(error => {
