8a6ac662be74acb48111651c4582949d1fd23e34
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-11 01:54:17 +0000

    completed reverse words

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,13 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  const reverseArray = [];
+  let reverseString = '';
+  const stringArray = string.split(' ');
+
+  for (var i = 0; i < stringArray.length; i++) {
+    var word = stringArray[i].split('').reverse().join('');
+    reverseArray.push(word);
+  }
+  reverseString = reverseArray.join(' ');
+  return reverseString;
+}
