f9c2fb9a0a4240db0b15ed534599ced59db96c25
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-12 23:40:59 +0000

    changed spacing

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -6,7 +6,6 @@ const reactHeader = React.createElement('h1', null, 'Hello, React!');
 console.log(reactHeader);
 
 const container = document.querySelector('#root');
-
 const root = ReactDOM.createRoot(container);
 
 root.render(reactHeader);
