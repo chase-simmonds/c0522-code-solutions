3048122b2f7de79b0d07842e5bd707f4e5ac9ce7
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-14 17:06:08 +0000

    edited render runction

--- react-events-and-state/src/index.jsx
+++ react-events-and-state/src/index.jsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 class CustomButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isClicked: false };
+    this.state = {
+      isClicked: false
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -13,21 +15,15 @@ class CustomButton extends React.Component {
   }
 
   render() {
-    if (!this.state.isClicked) {
-      return <button onClick={this.handleClick}>Click Me!</button>;
+    if (this.state.isClicked) {
+      return <button>Thanks!</button>;
     } else {
-      return <button onClick={this.handleClick}>Thank You!</button>;
+      return <button onClick={this.handleClick}>Click Me!</button>;
     }
   }
 }
 
-const element = (
-  <div>
-    <CustomButton />
-  </div>
-);
-
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(element);
+root.render(<CustomButton />);
