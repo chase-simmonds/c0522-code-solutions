29fb383125633ad2898e1636ce0897a80632f5aa
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-15 20:19:01 +0000

    react-rendering-lists - Chase Simmonds

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+function PokemonList(props) {
+  const pokedex = props.pokedex;
+  const ListItems = pokedex.map(pokemon =>
+    <ListItem key={pokemon.number} value={pokemon.name} />
+  );
+  return (
+    <ul>
+      {ListItems}
+    </ul>
+  );
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<PokemonList pokedex={pokedex} />);
