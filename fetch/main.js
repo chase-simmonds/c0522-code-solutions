142c7fe13653585c9cac0beec8f687f13b3a784a
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-18 21:48:54 +0000

    added catch and specified parameters in then arrow function

--- fetch/main.js
+++ fetch/main.js
@@ -1,7 +1,9 @@
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(users => console.log(users))
+  .catch(err => console.error('Fetch failed!', err));
 
 fetch('https://pokeapi.co/api/v2/pokemon/6')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(charizard => console.log(charizard))
+  .catch(err => console.error('Fetch failed!', err));
