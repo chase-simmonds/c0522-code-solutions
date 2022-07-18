6d19e1be9639063241d65c0479fed510ddcb4d7b
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-18 05:12:28 +0000

    progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,4 +1,24 @@
 /* exported reverseWords */
+
+/* define function with one parameter string
+
+create empty array for reversed array
+
+create empty string for reversed string
+
+split string at space and assign value to new variable
+
+loop through array created from splitting the string
+
+split string array at each character for each index, reverse it, then rejoin at
+each character then assign to variable
+
+push variable into the reverse array
+
+join the reverse array at space, then assign to reverse string
+
+return reversed string */
+
 function reverseWords(string) {
   const reverseArray = [];
   let reverseString = '';
