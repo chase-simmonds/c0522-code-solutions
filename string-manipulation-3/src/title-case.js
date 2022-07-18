a69b8b70950cb09a2b80dac119c5d504828012b9
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-18 06:13:55 +0000

    working on titlecase

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,10 @@
 /* exported titleCase */
+
+/* edge cases: JavaScript, API (hard code)
+
+hyphenated words
+
+minor words as long as not first word: and, or, nor, but, a, an, the, as, at,
+by, for, in, of, on, per, to
+
+define function with one parameter title */
