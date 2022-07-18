a11b5bb64e8f0fa6e590a3dc2df9e4f1d9279ff2
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-18 07:28:15 +0000

    finished all except zip

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,4 +1,14 @@
 /* exported equal */
+/* define function with two parameters first and second
+
+if first is equal to second, return true
+
+if length of first and length of second are not equal, return false
+
+loop through first and if first at i is not equal to second at i, return false
+
+else return true */
+
 function equal(first, second) {
   if (first === second) {
     return true;
--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,16 +1,29 @@
 /* exported flatten */
 
+/* create a new empty array
+
+loop through array then loop through array at i in code block
+
+check to see if array is an array at index i and if so push array at at first index
+that is at second index into the new array
+
+if it is not an array at first index, push array at first index into the new array
+
+return new array.
+
+*/
+
 function flatten(array) {
-  var flatArray = [];
+  var newArray = [];
   for (var i = 0; i < array.length; i++) {
     for (var k = 0; k < array[i].length; k++) {
       if (Array.isArray(array[i])) {
-        flatArray.push(array[i][k]);
+        newArray.push(array[i][k]);
       }
     }
     if (!Array.isArray(array[i])) {
-      flatArray.push(array[i]);
+      newArray.push(array[i]);
     }
   }
-  return flatArray;
+  return newArray;
 }
