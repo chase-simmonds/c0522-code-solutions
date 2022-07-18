494f6c6ad908964905079d2498b34e403fabc4eb
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-18 04:48:00 +0000

    progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,8 +1,27 @@
 /* exported isAnagram */
 
+/* define function with two parameters firststring & second string
+
+create empty string for both parameters and assign to variable
+
+loop through first string and add value at i to newfirst string variable if
+it isnt a space
+
+do the same for second string and add value to new second string
+
+for both newfirststring and newsecondstring, split at each character, sort,
+then rejoin at each character
+
+assign respective values to created variables
+
+run coniditonal if newfirststring is equal to newsecondstring return true
+
+else false
+*/
+
 function isAnagram(firstString, secondString) {
-  var newFirstString = '';
-  var newSecondString = '';
+  let newFirstString = '';
+  let newSecondString = '';
 
   for (var i = 0; i < firstString.length; i++) {
     if (firstString[i] !== ' ') {
