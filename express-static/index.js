985b62869063438562da79668bfd03ea7b829958
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-18 18:04:28 +0000

    changed variable names to match example

--- express-static/index.js
+++ express-static/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
+const publicPath = path.join(__dirname, 'public');
+const staticMiddleware = express.static(publicPath);
 
-const pathJoin = path.join(__dirname, 'public');
-
-const expressStatic = express.static(pathJoin);
-
-app.use(expressStatic);
+app.use(staticMiddleware);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
