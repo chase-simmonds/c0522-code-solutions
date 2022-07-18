147f74afae04fbcd44ab7e1b0cc937409f72f2ff
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-18 05:03:39 +0000

    progress

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,5 +1,18 @@
 /* exported isPalindromic */
 
+/* define function with one parameter string
+
+use replace all to remove spaces from string and assign new value to the string
+
+split the string at each character, reverse using method, then rejoin
+
+assign reversed value to new variable
+
+create conditional if reversed value is equal to string then return true
+
+else return false
+ */
+
 function isPalindromic(string) {
   string = string.replaceAll(' ', '');
   var reversed = string.split('').reverse().join('');
