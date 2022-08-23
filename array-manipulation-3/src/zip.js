3adbc258261d0238b25df69ea822b3416aaae18a
Chase Simmonds <chasexsimmonds@gmail.com>
2022-08-23 23:43:28 +0000

    array-manipulation-3 - Chase Simmonds

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -3,5 +3,18 @@
 /* define function with two parameters first and second
 
 creat new empty array
-
+apply math.min to both lengths of first and second parameters
+assign result to new variable
+loop through assigned variable
+push first at i and second at i into new array
+return new array
 */
+
+function zip(first, second) {
+  const newArray = [];
+  const shortest = Math.min(first.length, second.length);
+  for (let i = 0; i < shortest; i++) {
+    newArray.push([first[i], second[i]]);
+  }
+  return newArray;
+}
